76c506e8dcb0365921ad8efd2e140a502ca31a1d
KailashJangir <mrkailashjangir@yahoo.com>
2021-12-12 00:19:03 +0530

    Creating Relastionsheep

--- database/migrations/2021_12_11_182113_create_questions_table.php
+++ database/migrations/2021_12_11_182113_create_questions_table.php
@@ -17,7 +17,7 @@ class CreateQuestionsTable extends Migration
             $table->increments('id');
             $table->string('title');
             $table->string('slug');
-            $tabel->text('body');
+            $table->text('body');
             $table->integer('category_id')->unsigned();
             $table->integer('user_id')->unsigned();
             $table->timestamps();
